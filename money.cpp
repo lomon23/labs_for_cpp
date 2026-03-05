5cafd14d73b01f6eb0168122118ec3a442acba5a
lomon23 <lomontav22@gmail.com>
2026-03-06 01:07:30 +0200

    feat(money): updaterd architecture, added logs, and moving login into a separate function

--- money.cpp
+++ money.cpp
@@ -3,38 +3,16 @@
 #include <cmath>
 #include <map>
 #include <string>
-int main() {
-    double rest;
-    std::cin >> rest; 
-    std::vector<int> UAH = {20, 10, 5, 2, 1};
-    std::vector<int> COIN = {50, 25, 10, 5, 1};
-    
-    std::map<int, int, std::greater<int>> UAH_register{
-        {20,5},
-        {10,3},
-        {5,0},
-        {2,2},
-        {1,7},
-    };
-    std::map<int, int, std::greater<int>> COIN_register{
-        {50,5},
-        {25,3},
-        {10,4},
-        {5,2},
-        {1,7},
-    };
 
-    int totalCents = static_cast<int>(std::round(rest * 100));
-    
-    std::cout << totalCents << std::endl;
-    
-    int grivnas = totalCents / 100;
-    int coins = totalCents % 100;
-    
-    std::cout << grivnas << std::endl;
-    std::cout << coins << std::endl;
-    
-    
+
+void greedy_algorithm(
+    const std::vector<int>& UAH, 
+    const std::vector<int>& COIN, 
+    int grivnas, 
+    int coins,
+    std::map<int, int, std::greater<int>>& UAH_register, 
+    std::map<int, int, std::greater<int>>& COIN_register
+) {
     for (int bill : UAH) {
         int count = 0; 
 
@@ -58,4 +36,45 @@ int main() {
             std::cout << cent << " COIN --> " << count << std::endl;
         }
     }
+    if (grivnas > 0 || coins > 0) {
+    std::cout << "EMPTYYYY" << std::endl;
+}
+}
+int main() {
+    double rest;
+    std::cout << "enter rest -> ";
+    std::cin >> rest; 
+    std::vector<int> UAH = {20, 10, 5, 2, 1};
+    std::vector<int> COIN = {50, 25, 10, 5, 1};
+    
+    std::map<int, int, std::greater<int>> UAH_register{
+        {20,5},
+        {10,3},
+        {5,0},
+        {2,2},
+        {1,7},
+    };
+    std::map<int, int, std::greater<int>> COIN_register{
+        {50,5},
+        {25,3},
+        {10,4},
+        {5,2},
+        {1,7},
+    };
+
+    int totalCents = static_cast<int>(std::round(rest * 100));
+    std::cout << "========LOG=========" << std::endl;
+    std::cout << "converted to hundrets -> " << totalCents << std::endl;
+    
+    int grivnas = totalCents / 100;
+    int coins = totalCents % 100;
+    
+    std::cout << grivnas << std::endl;
+    std::cout << coins << std::endl;
+    std::cout << "====================" << std::endl;
+    
+    greedy_algorithm(UAH, COIN, grivnas, coins, UAH_register, COIN_register);
+    
+    
+    return 0;
 }
